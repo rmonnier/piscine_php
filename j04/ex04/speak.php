a5dea90699bd53a84549e12d3f1b0cc64d5f84f7
Robin MONNIER <rmonnier@e1z3r5p6.42.us.org>
2017-03-17 19:12:42 -0700

    j00 ex03 and j04 improved

--- j04/ex04/speak.php
+++ j04/ex04/speak.php
@@ -1,36 +1,38 @@
 <?php
 session_start();
-if ($_SESSION['loggued_on_user'] != "")
+
+if ($_SESSION['loggued_on_user'] == "")
+	exit("ERROR\n");
+
+if ($_POST['submit'] == 'OK' && strlen($_POST['msg']) > 0)
 {
-	if ($_POST['submit'] == 'OK' || strlen($_POST['msg']) > 0)
+	$chat_folder = "../htdocs/private";
+	$chat_path = $chat_folder . "/chat";
+
+	if (!file_exists($chat_folder))
+		mkdir($chat_folder, 0777, true);
+
+	if (!(file_exists($chat_path)))
 	{
-		$chat_folder = "../htdocs/private";
-		$chat_path = $chat_folder . "/chat";
-
-		if (!file_exists($chat_folder))
-			mkdir($chat_folder, 0777, true);
-
-		if (!(file_exists($chat_path)))
-		{
-			$chat = array();
-			$chat[] = array('login' => $_SESSION['loggued_on_user'], 'time' => time(), 'msg' => $_POST['msg']);
-			$chat_serialized = serialize($chat);
-			file_put_contents($chat_path, $chat_serialized);
-		}
-		else
-		{
-			$fd = fopen($chat_path, "c+");
-			flock($fd, LOCK_EX | LOCK_SH);
-			$array = file_get_contents($chat_path);
-			$chat_serialized = file_get_contents($chat_path);
-			$chat = unserialize($chat_serialized);
-			$chat[] = array('login' => $_SESSION['loggued_on_user'], 'time' => time(), 'msg' => $_POST['msg']);
-			$chat_serialized = serialize($chat);
-			file_put_contents($chat_path, $chat_serialized);
-			flock($fd, LOCK_UN);
-			fclose($fd);
-		}
+		$chat = array();
+		$chat[] = array('login' => $_SESSION['loggued_on_user'], 'time' => time(), 'msg' => $_POST['msg']);
+		$chat_serialized = serialize($chat);
+		file_put_contents($chat_path, $chat_serialized);
 	}
+	else
+	{
+		$fd = fopen($chat_path, "c+");
+		flock($fd, LOCK_EX | LOCK_SH);
+		$array = file_get_contents($chat_path);
+		$chat_serialized = file_get_contents($chat_path);
+		$chat = unserialize($chat_serialized);
+		$chat[] = array('login' => $_SESSION['loggued_on_user'], 'time' => time(), 'msg' => $_POST['msg']);
+		$chat_serialized = serialize($chat);
+		file_put_contents($chat_path, $chat_serialized);
+		flock($fd, LOCK_UN);
+		fclose($fd);
+	}
+}
 ?>
 <html>
 <head>
@@ -42,9 +44,3 @@ if ($_SESSION['loggued_on_user'] != "")
 	<input type="submit" name="submit" value="OK" />
 </form>
 </body></html>
-<?php
-}
-else {
-	exit("ERROR\n");
-}
-?>
